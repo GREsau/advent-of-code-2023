5a70923255ef27bf8ff9d9d70a5048a5b585b45b
Graham Esau <gesau@hotmail.co.uk>
2023-12-09 16:42:03 +0000

    Day 9: Haskell

---
+++ day9/part2.hs
@@ -0,0 +1,26 @@
+import Data.List
+
+isZeroes :: [Int] -> Bool
+isZeroes [] = True
+isZeroes (0:l) = isZeroes l
+isZeroes _ = False
+
+readInput = do
+  contents <- readFile "input.txt"
+  let input :: [[Int]] = map (map read) . map words . lines $ contents
+  return input
+
+pairs l = zip l (drop 1 l)
+
+differences l = map (uncurry (-)) $ pairs l
+
+
+findNext :: [Int] -> Int
+findNext l = if isZeroes l
+  then 0
+  else (head l) + findNext (differences l)
+
+part2 = do
+    input <- readInput
+    let answer = sum $ map findNext input
+    putStrLn ("Part 1: " ++ show answer)
